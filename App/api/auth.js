77f022ed25e89fd80e8aa187991913f2d16fe8ff
Jaskaran singh <jaskaransingh4704@gmail.com>
2021-08-24 09:21:20 +0530

    remove extra screen components

--- App/api/auth.js
+++ App/api/auth.js
@@ -3,9 +3,6 @@ import {apiClient} from './client';
 //Api endPoints
 const endPoint = {
   login: '/loginEmp',
-  signUp: '?action_name=signUp',
-  forgotPassword: '?action_name=forgotPassword',
-  updatePlayerId: '?action_name=updatePlayerId',
 };
 
 //Call Api Function
@@ -16,27 +13,7 @@ const signIn = async ({email, password}) => {
   });
 };
 
-const signUp = async ({username, mobile, email, password}) => {
-  return await apiClient.post(endPoint.signUp, {
-    username,
-    mobile,
-    email,
-    password,
-  });
-};
-
-const forgotPassword = async ({email}) => {
-  return await apiClient.post(endPoint.forgotPassword, {email});
-};
-
-const updatePlayerId = async ({id, player_id}) => {
-  return await apiClient.post(endPoint.updatePlayerId, {id, player_id});
-};
-
 //Export Functions
 export default {
   signIn,
-  signUp,
-  forgotPassword,
-  updatePlayerId,
 };
