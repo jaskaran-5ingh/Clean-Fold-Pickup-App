dec0859c7c64099883c613ea5b62546d1b3cb248
Jaskaran singh <jaskaransingh4704@gmail.com>
2021-08-24 09:21:20 +0530

    remove extra screen components

--- App/api/services.js
+++ App/api/services.js
@@ -2,74 +2,75 @@ import {apiClient} from './client';
 
 //Api endPoints
 const endPoint = {
+  createOrder: '/create-order',
+  doneDeliveryOrder: '/done-delivery',
+  donePendingOrder: '/done-pickup',
+  getCategoryProduct: '/getCategoryProduct/1',
   getDashboardData: '/get/dashboard',
   getOrdersList: '/get/orders/list',
-  getCategoryProduct: '/getCategoryProduct/1',
-  donePendingOrder: '/done-pickup',
-  doneDeliveryOrder: '/done-delivery',
-  getOrderDetailsById: '/get/order-detail',
-  updateDeliveredOrder: '/edit-order',
-  getUserByMobile: '/get/user',
   getOrderCategory: '/get/OrderCategory',
-  createOrder: '/create-order',
+  getOrderDetailsById: '/get/order-detail',
+  getUserByMobile: '/get/user',
+  updateDeliveredOrder: '/edit-order',
 };
 
 //Call Api Function
+
+const createOrder = saveOrderObject => {
+  return apiClient.post(endPoint.createOrder, saveOrderObject);
+};
+
 const getDashboardData = EmployeeID => {
   return apiClient.get(endPoint.getDashboardData + '/' + EmployeeID);
-};
-
-const getPendingOrdersList = EmployeeID => {
-  return apiClient.get(endPoint.getOrdersList + '/' + EmployeeID + '/1');
-};
-
-const getDeliveredOrdersList = EmployeeID => {
-  return apiClient.get(endPoint.getOrdersList + '/' + EmployeeID + '/2');
-};
-
-const getCategoryProduct = () => {
-  return apiClient.get(endPoint.getCategoryProduct);
-};
-
-const donePendingOrder = orderId => {
-  return apiClient.get(endPoint.donePendingOrder + '/' + orderId);
 };
 
 const doneDeliveryOrder = orderId => {
   return apiClient.get(endPoint.doneDeliveryOrder + '/' + orderId);
 };
 
+const donePendingOrder = orderId => {
+  return apiClient.get(endPoint.donePendingOrder + '/' + orderId);
+};
+
+const getCategoryProduct = () => {
+  return apiClient.get(endPoint.getCategoryProduct);
+};
+
+const getDeliveredOrdersList = EmployeeID => {
+  return apiClient.get(endPoint.getOrdersList + '/' + EmployeeID + '/2');
+};
+
 const getOrderDetailsById = orderId => {
   return apiClient.get(endPoint.getOrderDetailsById + '/' + orderId);
-};
-
-const updateDeliveredOrder = jsonData => {
-  return apiClient.post(endPoint.updateDeliveredOrder, jsonData);
-};
-
-const getUserByMobile = mobileNumber => {
-  return apiClient.get(endPoint.getUserByMobile + '/' + mobileNumber);
 };
 
 const getOrderCategory = () => {
   return apiClient.get(endPoint.getOrderCategory);
 };
 
-const createOrder = saveOrderObject => {
-  return apiClient.post(endPoint.createOrder, saveOrderObject);
+const getPendingOrdersList = EmployeeID => {
+  return apiClient.get(endPoint.getOrdersList + '/' + EmployeeID + '/1');
+};
+
+const getUserByMobile = mobileNumber => {
+  return apiClient.get(endPoint.getUserByMobile + '/' + mobileNumber);
+};
+
+const updateDeliveredOrder = jsonData => {
+  return apiClient.post(endPoint.updateDeliveredOrder, jsonData);
 };
 
 //Export Functions
 export default {
+  createOrder,
+  doneDeliveryOrder,
+  donePendingOrder,
   getDashboardData,
   getDeliveredOrdersList,
-  getPendingOrdersList,
   getCategoryProduct,
-  donePendingOrder,
-  doneDeliveryOrder,
-  getOrderDetailsById,
-  updateDeliveredOrder,
-  getUserByMobile,
+  getPendingOrdersList,
   getOrderCategory,
-  createOrder,
+  getOrderDetailsById,
+  getUserByMobile,
+  updateDeliveredOrder,
 };
