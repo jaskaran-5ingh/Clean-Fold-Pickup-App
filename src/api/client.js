089dad0e11b364d6c753e33ca22fe76ac3747143
Jaskaran Singh <42887065+JaskaranSingh4704@users.noreply.github.com>
2021-12-08 19:02:34 +0530

    Update client.js
    Update api base url

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import {create} from 'apisauce';
 
 // define the api
 const apiClient = create({
-  baseURL: 'https://cleanfold.in/backend/clean_fold/public/api',
+  baseURL: 'https://cleanfold.ripungupta.com/public/api',
   headers: {
     Authorization:
       'Bearer $2y$10$5OmYFG9clk67v8g2VO.YXOwSqVyX9MzPdtXhyQ4lHVjdpm62wuLMK',
